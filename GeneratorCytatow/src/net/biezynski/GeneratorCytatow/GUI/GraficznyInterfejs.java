c61dd2db0ce5d519d2c1d6c9831f1b8fd3864519
Wojciech Biezynski <wojciech.biezynski@onet.pl>
2018-07-13 13:59:34 +0200

    v.1.2 dziala ale kod jest beznadziejny

--- GeneratorCytatow/src/net/biezynski/GeneratorCytatow/GUI/GraficznyInterfejs.java
+++ GeneratorCytatow/src/net/biezynski/GeneratorCytatow/GUI/GraficznyInterfejs.java
@@ -4,22 +4,27 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.text.StyledEditorKit.FontSizeAction;
+
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+
 
 public class GraficznyInterfejs extends JFrame implements ActionListener {
 
 	private JPanel panel;
 	private JLabel tekst;
-//!!!!!!!!!	private JTextArea poleTekstowe;
 	private JButton przycisk;
-	private JButton przycisk2;
-	private JLabel losowyCytat;
+	private JTextPane textpane;
 
 	public GraficznyInterfejs() {
 
@@ -38,39 +43,82 @@ public class GraficznyInterfejs extends JFrame implements ActionListener {
 		tekst.setFont(new Font("TimesRoman", Font.PLAIN, fontsize));
 		tekst.setBounds(10, 10, 800, 25);
 
-	//!!!!!	poleTekstowe = new JTextArea();
-	//!!!!!	poleTekstowe.setBounds(10, 60, 200, 25);
+	
 
 		przycisk = new JButton("Generuj");
-		przycisk.setBounds(10, 120, 100, 30);
+		przycisk.setBounds(10, 50, 100, 30);
 		przycisk.addActionListener(this);
 
-		losowyCytat = new JLabel("Tutaj bedzie sie pojawial tekst z losowym cytatem");
-		losowyCytat.setFont(new Font("NowyRoman", Font.ITALIC, fontsize));
-		losowyCytat.setBounds(10, 10, 800, 400);
+	
+		
+		textpane= new JTextPane();
+		textpane.setFont(new Font("Roman", Font.BOLD,25));
+		textpane.setBounds(50, 110, 700, 600);
+		textpane.setBackground(new Color(143, 188, 143));
+		
+		
+		
+		//te 4 linijki ponizej maja wysrodkowowac tekst
+		StyledDocument doc = textpane.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+		
+		
+		
+		
+		
 
 		add(panel);
-	//!!!!!	panel.add(poleTekstowe);
+	
 		panel.add(tekst);
 		panel.add(przycisk);
-	//	panel.add(losowyCytat);
 		
+
 	}
 
-	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) { 
 
-	
 		if (e.getSource() == przycisk) {
+
+			List<String> ListaCytatow = new ArrayList<String>();
+			ListaCytatow.add("Tylko ten nie pope³nia b³êdów, kto nic nie robi. -Napoleon Bonaparte");
+			ListaCytatow.add(
+					"S¹ dwa sposoby na tworzenie projektu oprogramowania. "
+					+ "Jednym jest stworzenie go "
+					+ "tak prostym, ¿eby nie by³o w nim ¿adnych oczywistych braków. Drugim stworzenie go "
+					+ "tak skomplikowanym, ¿eby nie by³o w nim ¿adnych oczywistych braków - Hoare");
+			ListaCytatow.add("Wiêkszoœæ z was zna ju¿ cnoty programistów. S¹ to oczywiœcie: "
+					+ "lenistwo, niecierpliwoœæ i pycha- Larry Wall");
+			ListaCytatow.add("Nie obchodzi mnie to, ¿e to nie dzia³a na twoim komputerze! Nie produkujemy"
+					+ " twoich komputerów! - Vidiu Platon");
+			ListaCytatow.add("Programowanie w C jest jak szybki taniec na œwie¿o pastowanej pod³odze "
+					+ "wœród ludzi trzymaj¹cych ¿yletki- Waldi Ravens");
+			ListaCytatow.add("Zawsze pisz kod tak, jakby goœæ, który ma siê nim zajmowaæ by³ agresywnym "
+					+ "psychopat¹, który wie, gdzie mieszkasz- Martin Golding");
+			ListaCytatow.add("Komputer zas³u¿y na miano inteligentnego, je¿eli spowoduje u cz³owieka "
+					+ "przekonanie, ¿e jest cz³owiekiem.- Alan Turing");
+			ListaCytatow.add("W matematyce rzeczy siê nie pojmuje, tylko siê do nich przyzwyczaja.- "
+					+ "John von Neumann");
+
+			int Losowaliczba = (int) (Math.random() * ListaCytatow.size());
+
+			String losCytat = ListaCytatow.get(Losowaliczba);
+
+		
 			
-					
-			
-			panel.add(losowyCytat);
 			
 			
-			losowyCytat.setText("Tutaj bedzie pojawial sie losowy Cytat");
 			
+			
+			
+			
+			
+			
+			
+			panel.add(textpane);
+			textpane.setText(losCytat);
 		}
 
 	}
--- GeneratorCytatow/src/net/biezynski/GeneratorCytatow/glowny/Glowna.java
+++ GeneratorCytatow/src/net/biezynski/GeneratorCytatow/glowny/Glowna.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import net.biezynski.GeneratorCytatow.GUI.GraficznyInterfejs;
 
+public class Glowna {
 
-public class glowna{
-
-	
 	public static void main(String[] args) {
+
 		
 		
+		/*
 		List<String> ListaCytatow = new ArrayList<String>();
 		ListaCytatow.add("Losowy Cytat nr 1");
 		ListaCytatow.add("Losowy Cytat nr 2");
@@ -21,17 +21,19 @@ public class glowna{
 		ListaCytatow.add("Losowy Cytat nr 6");
 		ListaCytatow.add("Losowy Cytat nr 7");
 		ListaCytatow.add("Losowy Cytat nr 8");
-		
-	//	System.out.println(ListaCytatow.size());
-		
-	int Losowaliczba = (int) (Math.random()* ListaCytatow.size() );
-		
-		System.out.println(ListaCytatow.get(Losowaliczba));
-		
+
+		// System.out.println(ListaCytatow.size());
+
+		int Losowaliczba = (int) (Math.random() * ListaCytatow.size());
+
+		String losCytat = ListaCytatow.get(Losowaliczba);
+		// System.out.println(ListaCytatow.get(Losowaliczba));
+		System.out.println(losCytat);
+		*/
 		
 		
 		new GraficznyInterfejs().setVisible(true);
-		
+
 	}
 
 }
